fc9db9ef432ca529f16cc277f4633f831ae8e075
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-11-28 14:41:49 +0530

    Code format changed

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -7,32 +7,35 @@
 //
 // }
 
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 @Component({
   selector: 'comp2',
   templateUrl: 'comp2.html'
 })
 export class Comp2 {
-clickstatus="Don't click me please!";
-isdisabled=true;
-inputstring="";
-fun=()=> {
-  if (this.clickstatus === "Don't click me please!") {
-    this.clickstatus = 'Click Me now !';
-    this.isdisabled=true;
+  clickstatus = 'Don\'t click me please!';
+  isdisabled = true;
+  inputstring = '';
+  fun = () => {
+    if (this.clickstatus === 'Don\'t click me please!') {
+      this.clickstatus = 'Click Me now !';
+      this.isdisabled = true;
+    }
+    else if (this.clickstatus === 'Click Me now !') {
+      this.clickstatus = 'Don\'t click me please!';
+      this.isdisabled = false;
+    }
+  };
+
+  constructor() {
+    setTimeout(this.fun
+      , 2000);
   }
-  else if (this.clickstatus === 'Click Me now !') {
-    this.clickstatus = "Don't click me please!";
-    this.isdisabled=false;
+
+  onInput(event: Event) {
+    console.log(event);
+    this.inputstring = event.target.value;
   }
-}
-constructor(){
-  setTimeout(this.fun
-  ,2000);
-}
-onInput(event:Event){
-  console.log(event);
-  this.inputstring=event.target.value;
-}
 }
 
